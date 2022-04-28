c6a821b3618fa10d66082f077988bdcfbcf3954e
Haoqi Shan Xu <104140318+al-7555056@users.noreply.github.com>
2022-04-28 13:04:44 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    si += 1
+    if (si == 2) {
+        si = 0
+    }
+})
+let si = 0
 let strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
 music.setVolume(0)
 basic.forever(function () {
@@ -7,6 +14,29 @@ basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 10) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
         basic.pause(500)
+        maqueen.motorStop(maqueen.Motors.All)
+        music.setVolume(255)
+        for (let index = 0; index < 4; index++) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
+            basic.pause(200)
+            strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+            basic.pause(200)
+        }
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        music.setVolume(0)
+        if (si == 1) {
+            if (randint(0, 1) == 1) {
+                maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+                basic.pause(500)
+                maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            } else {
+                maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+                basic.pause(500)
+                maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            }
+        }
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     }
